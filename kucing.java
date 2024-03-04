89fd9abfdd3f4b6b3a55d18584478b3cace873dc
farisalaudin <farisalaudin02@gmail.com>
2024-03-04 12:28:05 +0700

    Initial commit

---
+++ kucing.java
@@ -0,0 +1,50 @@
+public class kucing {
+    // Fields (Properties/Attributes)
+    private String nama;
+    private String warna;
+    private int umur;
+    private boolean lapar;
+
+    // Constructor
+    public kucing(String nama, String warna, int umur) {
+        this.nama = nama;
+        this.warna = warna;
+        this.umur = umur;
+        this.lapar = true; // By default, the cat is hungry
+    }
+
+    // Methods (Behaviors)
+    public void meow() {
+        System.out.println(nama + " says: stardenburdenhardenbart!");
+    }
+
+    public void makan(String makanan) {
+        System.out.println(nama + " mangan " + makanan);
+        lapar = false;
+    }
+
+    public void tidur() {
+        System.out.println(nama + " turu sek");
+        lapar = true; // The cat might wake up hungry
+    }
+
+    public void info() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Warna: " + warna);
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Lapar: " + (lapar ? "meong" : "meow"));
+    }
+
+    // Main method to test the Kucing class
+    public static void main(String[] args) {
+        // Create a Kucing object
+        kucing myCat = new kucing("Muhammad sumbul", "pelangi", 3);
+
+        // Call methods to interact with the cat
+        myCat.info(); // Display initial info
+        myCat.meow();
+        myCat.makan("wiskas");
+        myCat.tidur();
+        myCat.info(); // Display updated info after actions
+    }
+}
